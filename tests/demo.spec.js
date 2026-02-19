e26881149f2e25ae98ea64276c59e82d2a3fb823
kartik iih <kartik.iihglobal@gmail.com>
2026-02-19 15:06:11 +0530

    Initial Playwright automation setup

---
+++ tests/demo.spec.js
@@ -0,0 +1,6 @@
+const { test, expect } = require('@playwright/test');
+
+test('open google', async ({ page }) => {
+  await page.goto('https://www.google.com');
+  await expect(page).toHaveTitle(/Google/);
+});
